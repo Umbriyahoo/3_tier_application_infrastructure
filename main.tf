b464449231215b60cbbdc7973e38bf52028379e0
Umbriyahoo <70366926+Umbriyahoo@users.noreply.github.com>
2022-09-21 13:04:53 +0300

    adding-code

--- main.tf
+++ main.tf
@@ -98,6 +98,67 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
+resource "aws_autoscaling_policy" "cpu-scaleup" {
+  name                   = "cpu-scaleup"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu-alarm-scaleup" {
+  alarm_name          = "cpu-alarm-scaleup"
+  alarm_description   = "cpu-alarm-scaleup"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "75"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.asg.name
+  }
+
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.cpu-scaleup.arn]
+}
+
+
+
+resource "aws_autoscaling_policy" "cpu-scaledown" {
+  name                   = "cpu-scaledown"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "cpu-alarm-scaledown" {
+  alarm_name          = "cpu-alarm-scaledow"
+  alarm_description   = "cpu-alarm-scaledow"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "5"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.asg.name
+  }
+
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.cpu-scaledown.arn]
+}
+
+
 
 
 resource "aws_db_instance" "RDS" {
